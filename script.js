6724346f6af797ff010ca4bde43e2465f519852f
sofiaamosqueraa <sofiaam0202@gmail.com>
2023-11-29 15:28:23 +0000

    novaidea

---
+++ script.js
@@ -0,0 +1,36 @@
+
+var algorithmList = document.getElementById('algorithmList');
+var searchInput = document.getElementById('searchInput');
+
+
+function searchAlgorithm() {
+    var searchTerm = searchInput.value.toLowerCase();
+
+    
+    for (var i = 0; i < algorithmList.children.length; i++) {
+        var algorithm = algorithmList.children[i];
+        var algorithmText = algorithm.textContent.toLowerCase();
+
+        if (algorithmText.includes(searchTerm)) {
+            algorithm.style.display = 'block';  
+        } else {
+            algorithm.style.display = 'none';   
+        }
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var liItems = document.querySelectorAll('.li-1');
+
+    liItems.forEach(function (li) {
+        li.addEventListener('mouseover', function () {
+            this.style.backgroundColor = '#a9c4db'; 
+        });
+
+        li.addEventListener('mouseout', function () {
+            this.style.backgroundColor = ''; 
+        });
+    });
+});
